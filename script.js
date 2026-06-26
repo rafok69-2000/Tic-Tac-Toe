1ccb052bd116f9014f4bc32c767a238b31102d16
Rafael Santos Ciriaco <santosrafael9279@gmail.com>
2026-06-26 10:19:17 -0600

    implement full tic tac toe game flow

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ const gameBoard = (() => {
     const placeMarker = (row, col, symbol) => {
         if (board[row][col] === '') {
             board[row][col] = symbol;
+            return true;
         } else {
-            console.log('The cell is not empty');
+            return false;
         }
     };
 
@@ -30,29 +31,27 @@ const gameBoard = (() => {
 
 })();
 
-console.log(gameBoard.getGameBoard());
-
 function createPlayer(name, symbol) {
     let score = 0;
 
     const getScore = () => score;
     const increaseScore = () => { score++ };
+    const resetScore = () => score = 0;
 
     return {
         name,
         getScore,
         symbol,
-        increaseScore
+        increaseScore,
+        resetScore
     };
-}
+};
 
-const printNewRound = () => {
-    gameBoard.getGameBoard();
-    console.log
-}
-
+/*
 const player1 = createPlayer('Rafael', 'X');
 const player2 = createPlayer('Ana', 'O');
+const game = gameController(player1, player2);
+*/
 
 function gameController(player1, player2) {
 
@@ -161,39 +160,163 @@ function gameController(player1, player2) {
 
     const getCurrentPlayer = () => currentPlayer;
 
-    const playRound = (row, col) => {
-        if (getCurrentPlayer().getScore() === 0) {
-            console.log(`${getCurrentPlayer().name} is your turn`);
-            console.log(gameBoard.getGameBoard());
-            gameBoard.placeMarker(row, col, getCurrentPlayer().symbol);
+    let gameOver = false;
+    let roundOver = false;
 
-            const hasWinner = winner();
-            if (hasWinner === false) {
-                tie();
+    const getRoundOver = () => roundOver;
+    const getGameOver = () => gameOver;
+
+    const resetGameOver = () => gameOver = false;
+    const resetRoundOver = () => roundOver = false;
+
+    const playRound = (row, col) => {
+        if (gameOver === false) {
+            if (roundOver === false) {
+                console.log(`${getCurrentPlayer().name} is your turn`);
+                console.log(gameBoard.getGameBoard());
+                let validMovement = gameBoard.placeMarker(row, col, getCurrentPlayer().symbol);
+                if (validMovement === true) {
+                    const hasWinner = winner();
+                    if (hasWinner === false) {
+                        switchTurn();
+                        tie();
+                    }
+                    if (hasWinner == true) {
+                        let gameHasWinner = gameWinner();
+                        if (gameHasWinner === true) {
+                            console.log('Game Over');
+                            gameOver = true;
+                        } else {
+                            switchTurn();
+                            roundOver = true;
+                        }
+                    }
+                }
+                else {
+                    console.log('Try again')
+                }
             }
-            switchTurn();
-        } else {
-            console.log('Game Over');
         }
 
+    }
 
+    const gameWinner = () => {
+        const playerScore = getCurrentPlayer().getScore();
+        if (playerScore === 3) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+
+    const newRound = () => {
+        if (gameOver === false) {
+            gameBoard.resetGameBoard();
+            roundOver = false;
+        }
     };
-
-    playRound(0, 0);//rafa
-    playRound(0, 1);//ana
-    playRound(0, 2);//rafa
-    playRound(1, 0);//ana
-    playRound(1, 1);//rafa
-    playRound(1, 2);//ana
-    playRound(2, 1);//rafa
-    playRound(2, 0);//ana
-    playRound(2, 2);//rafa
-
 
     return {
         playRound,
-        getCurrentPlayer
+        getCurrentPlayer,
+        newRound,
+        getGameOver,
+        resetGameOver,
+        resetRoundOver
     };
 }
 
-gameController(player1, player2);
+
+function domController() {
+
+    const board = document.getElementById('boardContainer');
+
+    const renderBoard = () => {
+
+
+        for (let i = 0; i < gameBoard.getGameBoard().length; i++) {       // Itera sobre las filas
+            for (let j = 0; j < gameBoard.getGameBoard()[i].length; j++) {  // Itera sobre las columnas
+                const newCell = document.createElement('div');
+                newCell.classList.add('cell');
+                newCell.textContent = gameBoard.getGameBoard()[i][j];
+                newCell.dataset.row = i;
+                newCell.dataset.columns = j;
+                newCell.addEventListener('click', function () {
+                    if (game.getGameOver()) {
+                        return;
+                    }
+                    board.innerHTML = '';
+                    game.playRound(i, j);
+                    renderBoard();
+                });
+                board.appendChild(newCell);
+            }
+        }
+
+    };
+
+    const btnReset = document.getElementById('nextRound');
+    btnReset.addEventListener('click', () => {
+        game.newRound();
+        board.innerHTML = '';
+        renderBoard();
+    });
+
+    const btnNewGame = document.getElementById('resetGame');
+    btnNewGame.addEventListener('click', () => {
+        gameBoard.resetGameBoard();
+        game.resetGameOver();
+        game.resetRoundOver();
+        player1.resetScore();
+        player2.resetScore();
+        board.innerHTML = '';
+        renderBoard();
+    });
+
+    const btnNames = document.getElementById('showModal');
+    const btnSave = document.getElementById('save');
+    const input1 = document.getElementById('playerName1')
+    const input2 = document.getElementById('playerName2');
+    const meModal = document.getElementById('modal');
+    const btnClose = document.getElementById('close-modal');
+
+    btnNames.addEventListener('click', () => {
+        meModal.classList.add('show');
+        input1.value = '';
+        input2.value = '';
+    });
+
+    btnClose.addEventListener('click', () => {
+        meModal.classList.remove('show');
+    });
+
+    btnSave.addEventListener('click', () => {
+        const name1 = input1.value;
+        const name2 = input2.value;
+
+        player1 = createPlayer(name1, 'X');
+        player2 = createPlayer(name2, 'O');
+
+        game = gameController(player1, player2);
+
+        board.innerHTML = '';
+        gameBoard.resetGameBoard();
+        renderBoard();
+
+        meModal.classList.remove('show');
+
+    });
+
+    return {
+        renderBoard
+    };
+};
+
+let player1 = createPlayer('Player 1', 'X');
+let player2 = createPlayer('Player 2', 'O');
+
+let game = gameController(player1, player2);
+
+const dom = domController();
+dom.renderBoard();
